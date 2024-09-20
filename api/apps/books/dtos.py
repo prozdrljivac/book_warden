abf4a4e6f87d2c0415b10b249589b04608a57b9b
Petar Cevriz <pcevriz95@gmail.com>
2024-09-20 06:22:35 +0200

    BW-80012625: Add book update api and corresponding dto

--- api/apps/books/dtos.py
+++ api/apps/books/dtos.py
@@ -5,3 +5,9 @@ class CreateBookDto(BaseModel):
     title: str
     description: str | None = None
     author: str
+
+
+class UpdateBookDto(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    author: str | None = None
