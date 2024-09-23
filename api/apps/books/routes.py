8624673a7b01847438001daf71f49a0b7b961fa3
Petar Cevriz <pcevriz95@gmail.com>
2024-09-23 05:11:32 +0200

    BW-80012642: Add simple delete book API

--- api/apps/books/routes.py
+++ api/apps/books/routes.py
@@ -44,3 +44,8 @@ async def create_book(create_book_dto: CreateBookDto):
 async def update_book(book_id: int, update_book_dto: UpdateBookDto):
     print(book_id)
     return update_book_dto
+
+
+@router.delete("/{book_id}")
+async def delete_book(book_id: int):
+    return book_id
