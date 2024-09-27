8ff43050c194e0bb4934f38a0e3d6eedbe0eb2d5
Petar Cevriz <pcevriz95@gmail.com>
2024-09-27 07:33:40 +0200

    BW-81144289: Add get book from db, adjust apis to use context, split dtos to request and response

--- api/apps/books/routes.py
+++ api/apps/books/routes.py
@@ -1,8 +1,14 @@
 import sqlite3
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 
-from apps.books.dtos import CreateBookDto, GetBookDto, ListBookDto, UpdateBookDto
+from apps.books.dtos import (
+    CreateBookRequestDto,
+    CreateBookResponseDto,
+    GetBookResponseDto,
+    ListBookResponseDto,
+    UpdateBookDto,
+)
 
 router = APIRouter(
     prefix="/books",
@@ -11,18 +17,17 @@ router = APIRouter(
 
 @router.get("/")
 async def get_books():
-    db_connection = sqlite3.connect("db/dev.db")
-    db_cursor = db_connection.cursor()
-    result = db_cursor.execute(
-        """
-        SELECT id, title, description, author FROM books;
-        """
-    )
-    books = result.fetchall()
-    db_cursor.close()
-    db_connection.close()
+    with sqlite3.connect("db/dev.db") as db_connection:
+        db_cursor = db_connection.cursor()
+        result = db_cursor.execute(
+            """
+            SELECT id, title, description, author FROM books;
+            """
+        )
+        books = result.fetchall()
+
     return [
-        ListBookDto(
+        ListBookResponseDto(
             id=book[0],
             title=book[1],
             description=book[2],
@@ -34,33 +39,52 @@ async def get_books():
 
 @router.get("/{book_id}")
 async def get_book(book_id: int):
-    return GetBookDto(
-        id=book_id,
-        title="Hello World!",
-        description="Short description",
-        author="Petar Cevriz",
+    with sqlite3.connect("db/dev.db") as db_connection:
+        db_cursor = db_connection.cursor()
+        result = db_cursor.execute(
+            "SELECT id, title, description, author FROM books WHERE id=?",
+            (book_id,),
+        )
+        book = result.fetchone()
+
+        if not book:
+            return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    return GetBookResponseDto(
+        id=book[0],
+        title=book[1],
+        description=book[2],
+        author=book[3],
     )
 
 
-@router.post("/")
-async def create_book(create_book_dto: CreateBookDto):
-    db_connection = sqlite3.connect("db/dev.db")
-    db_cursor = db_connection.cursor()
-    db_cursor.execute(
-        """
-        INSERT INTO books (title, description, author) VALUES (?, ?, ?)
-        """,
-        (
-            create_book_dto.title,
-            create_book_dto.description,
-            create_book_dto.author,
-        ),
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_book(create_book_request_dto: CreateBookRequestDto):
+    with sqlite3.connect("db/dev.db") as db_connection:
+        db_cursor = db_connection.cursor()
+        db_cursor.execute(
+            """
+            INSERT INTO books (title, description, author) VALUES (?, ?, ?)
+            """,
+            (
+                create_book_request_dto.title,
+                create_book_request_dto.description,
+                create_book_request_dto.author,
+            ),
+        )
+        book_id = db_cursor.lastrowid
+        db_cursor.execute(
+            "SELECT id, title, description, author FROM books WHERE id = ?",
+            (book_id,),
+        )
+        book = db_cursor.fetchone()
+        db_connection.commit()
+    return CreateBookResponseDto(
+        id=book[0],
+        title=book[1],
+        description=book[2],
+        author=book[3],
     )
-    db_connection.commit()
-    db_cursor.close()
-    db_connection.close()
-    create_book_dto.model_dump(exclude=["id"])
-    return create_book_dto
 
 
 @router.patch("/{book_id}")
@@ -69,6 +93,6 @@ async def update_book(book_id: int, update_book_dto: UpdateBookDto):
     return update_book_dto
 
 
-@router.delete("/{book_id}")
+@router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(book_id: int):
     return book_id
