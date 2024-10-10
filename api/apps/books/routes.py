9010174c815a164c6cfff98865d7cde33f47084a
Petar Ćevriz <62151376+prozdrljivac@users.noreply.github.com>
2024-10-10 04:34:50 +0200

    BW-81144349: Update book information on patch api (#17)
    * BW-81144349: Add update dtos, remove async until I figure out how it is used here, add draft work for partial update
    
    * BW-81144349: Fix bug where values that were not in the request were set to None

--- api/apps/books/routes.py
+++ api/apps/books/routes.py
@@ -7,7 +7,8 @@ from apps.books.dtos import (
     CreateBookResponseDto,
     GetBookResponseDto,
     ListBookResponseDto,
-    UpdateBookDto,
+    UpdateBookRequestDto,
+    UpdateBookResponseDto,
 )
 
 router = APIRouter(
@@ -16,7 +17,7 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_books():
+def get_books():
     with sqlite3.connect("db/dev.db") as db_connection:
         db_cursor = db_connection.cursor()
         result = db_cursor.execute(
@@ -38,7 +39,7 @@ async def get_books():
 
 
 @router.get("/{book_id}")
-async def get_book(book_id: int):
+def get_book(book_id: int):
     with sqlite3.connect("db/dev.db") as db_connection:
         db_cursor = db_connection.cursor()
         result = db_cursor.execute(
@@ -59,7 +60,7 @@ async def get_book(book_id: int):
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_book(create_book_request_dto: CreateBookRequestDto):
+def create_book(create_book_request_dto: CreateBookRequestDto):
     with sqlite3.connect("db/dev.db") as db_connection:
         db_cursor = db_connection.cursor()
         db_cursor.execute(
@@ -87,14 +88,50 @@ async def create_book(create_book_request_dto: CreateBookRequestDto):
     )
 
 
+# TODO - check if user can send request without update_book_dto
 @router.patch("/{book_id}")
-async def update_book(book_id: int, update_book_dto: UpdateBookDto):
-    print(book_id)
-    return update_book_dto
+def update_book(book_id: int, update_book_dto: UpdateBookRequestDto):
+    with sqlite3.connect("db/dev.db") as db_connection:
+        db_cursor = db_connection.cursor()
+        result = db_cursor.execute(
+            "SELECT id, title, description, author FROM books WHERE id=?",
+            (book_id,),
+        )
+        book = result.fetchone()
+
+        if not book:
+            return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+        # This only makes sense if our values cannot be None in the DB
+        update_book_dct = update_book_dto.model_dump(exclude_none=True)
+        update_fields = [f"{k} = ?" for k in update_book_dct.keys()]
+        params = [v for v in update_book_dct.values()]
+
+        if update_fields:
+            query = f"UPDATE books SET {', '.join(update_fields)} WHERE id = ?"
+            # TODO need a better way to pull this id
+            params.append(book[0])
+
+            db_cursor.execute(query, tuple(params))
+            db_connection.commit()
+
+            updated_book = db_cursor.execute(
+                "SELECT id, title, description, author FROM books WHERE id=?",
+                (book_id,),
+            ).fetchone()
+
+            return UpdateBookResponseDto(
+                id=updated_book[0],
+                title=updated_book[1],
+                description=updated_book[2],
+                author=updated_book[3],
+            )
+    # TODO check if this is a correct response
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_book(book_id: int):
+def delete_book(book_id: int):
     with sqlite3.connect("db/dev.db") as db_connection:
         db_cursor = db_connection.cursor()
         db_cursor.execute(
