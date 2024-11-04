d19d15afc5279bccac933358b75a790ef8c49501
Petar Ćevriz <62151376+prozdrljivac@users.noreply.github.com>
2024-11-04 18:59:24 +0100

    BW-83155611: Book Repository not typed properly (#21)
    * BW-83155611: Separate dtos and repository
    
    * BW-83155611: Fix create so it does not need description in the request, fix update so it does not change values that are not provided in the request

--- api/apps/books/routes.py
+++ api/apps/books/routes.py
@@ -17,22 +17,22 @@ router = APIRouter(
 )
 
 
-# TODO look into depends more
 def get_book_repository():
     return BookRepository()
 
 
 @router.get("/", response_model=List[ListBookResponseDto])
 def get_books(repository: BookRepository = Depends(get_book_repository)):
-    return repository.get_books()
+    books = repository.get_books()
+    return [ListBookResponseDto(**book.model_dump()) for book in books]
 
 
 @router.get("/{book_id}", response_model=GetBookResponseDto)
 def get_book(book_id: int, repository: BookRepository = Depends(get_book_repository)):
-    book = repository.get_book(book_id)
+    book = repository.get_book(id=book_id)
     if not book:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
-    return book
+    return GetBookResponseDto(**book.model_dump())
 
 
 @router.post(
@@ -44,7 +44,12 @@ def create_book(
     create_book_request_dto: CreateBookRequestDto,
     repository: BookRepository = Depends(get_book_repository),
 ):
-    return repository.create_book(create_book_request_dto)
+    created_book = repository.create_book(
+        title=create_book_request_dto.title,
+        description=create_book_request_dto.description,
+        author=create_book_request_dto.author,
+    )
+    return CreateBookResponseDto(**created_book.model_dump())
 
 
 @router.patch("/{book_id}", response_model=UpdateBookResponseDto)
@@ -53,14 +58,24 @@ def update_book(
     update_book_dto: UpdateBookRequestDto,
     repository: BookRepository = Depends(get_book_repository),
 ):
-    updated_book = repository.update_book(book_id, update_book_dto)
+    updated_book = repository.update_book(
+        id=book_id,
+        title=update_book_dto.title,
+        description=update_book_dto.description,
+        author=update_book_dto.author,
+    )
     if not updated_book:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
-    return updated_book
+
+    return UpdateBookResponseDto(**updated_book.model_dump())
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_book(
     book_id: int, repository: BookRepository = Depends(get_book_repository)
 ):
-    repository.delete_book(book_id)
+    book = repository.get_book(id=book_id)
+    if not book:
+        return Response(status_code=status.HTTP_404_NOT_FOUND)
+
+    repository.delete_book(id=book.id)
